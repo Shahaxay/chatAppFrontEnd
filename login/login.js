417ce7c293655bdeee94d11181411e1522347913
Akshayshah47 <removeit61@gmail.com>
2023-07-04 23:44:49 +0530

    loginpage frontend added

---
+++ login/login.js
@@ -0,0 +1,17 @@
+const login_form=document.getElementById('login_form');
+const email=document.getElementById('login_form_email');
+const password=document.getElementById('login_form_password');
+
+login_form.addEventListener('submit',async (e)=>{
+    e.preventDefault();
+    let login_cred={
+        email:email.value,
+        password:password.value
+    };
+    try{
+        await axios.post('http://localhost:3000/user/login',login_cred);
+    }
+    catch(err){
+        console.log(err);
+    }
+})
