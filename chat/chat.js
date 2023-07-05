c57ac45d8319426e5d0ad6a824712ce1e33083d8
Akshayshah47 <removeit61@gmail.com>
2023-07-05 20:21:17 +0530

    frontend for storing 10 most recent msg in the local storage

--- chat/chat.js
+++ chat/chat.js
@@ -35,12 +35,34 @@ send_msg_form.addEventListener('submit',async (e)=>{
 //fetching all the message from the server
 async function fetchAllMessages(){
     chat_dest.innerHTML='';
+    let message_array=localStorage.getItem('message_array');
+    // console.log(message_array.length);
+    let last_msg_id=0;
+    if(message_array){
+        message_array=JSON.parse(message_array);
+        let last_index=message_array.length-1;
+        if(message_array.length>0){
+            last_msg_id=message_array[last_index].id;
+        }
+    }
+    console.log(last_msg_id);
     try{
-        const messages=await axios.get('http://localhost:3000/get-messages',{headers:{token:localStorage.getItem('token')}});
-        console.log(messages);
-        messages.data.forEach(message=>{
+        const messages=await axios.get(`http://localhost:3000/get-messages?lastMessageId=${last_msg_id}`,{headers:{token:localStorage.getItem('token')}});
+        // console.log(messages);
+        let newMergedArray;
+        if(message_array){
+            newMergedArray=message_array.concat(messages.data);
+        }else{
+            newMergedArray=messages.data;
+        }
+        newMergedArray.forEach(message=>{
             addMessage(message.name,message.message);
         });
+        //storing 10 recent message in the localStorage //for previous msg use older button
+        if(newMergedArray.length>10){
+            newMergedArray=newMergedArray.slice(newMergedArray.length-10);
+        }  
+        localStorage.setItem('message_array',JSON.stringify(newMergedArray));
     }
     catch(err){
         console.log(err);
@@ -52,3 +74,5 @@ async function fetchAllMessages(){
 
 //setting time interval for fetching all the message
 setInterval(fetchAllMessages,1000);
+
+
